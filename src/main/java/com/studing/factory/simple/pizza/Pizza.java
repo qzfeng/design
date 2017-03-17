2e44aaef4348a17e940f0b29f09f8753d03b6b36
qzfeng <1123824550@qq.com>
2017-03-17 09:15:32 +0800

    add Pizza.java at simple factory

---
+++ src/main/java/com/studing/factory/simple/pizza/Pizza.java
@@ -0,0 +1,43 @@
+package com.studing.factory.simple.pizza;
+
+import java.util.ArrayList;
+
+/**
+ * Created by fengqz on 2017-3-16.
+ */
+public abstract class Pizza {
+    String name;
+    String dough; // 面团
+    String sauce; // 酱
+    ArrayList toppings = new ArrayList();
+
+    public void prepare() {
+        System.out.println("Preparing " + name);
+        System.out.println("Tossing dough ...");
+        System.out.println("Adding sauce ...");
+        System.out.println("Adding topping:");
+        for (int i = 0; i < toppings.size(); i++) {
+            System.out.println("   " + toppings.get(i));
+        }
+    }
+
+    public void bake() {
+        System.out.println("Bake for 25 minutes at 350");
+    }
+
+    public void cut() {
+        System.out.println("Cutting the pizza into diagonal slices");
+    }
+
+    public void box() {
+        System.out.println("Place pizza in official PizzaStore box");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
